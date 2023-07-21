edecfeff7e705054fb03683fbb69f93a531f7ef9
Lucubrator <1318645809@qq.com>
2023-07-21 15:31:53 +0800

    更改图片清晰度并实现从数据库动态上传图片

--- item/views.py
+++ item/views.py
@@ -1,7 +1,10 @@
 from item import templates
 from django.shortcuts import render, HttpResponse
 from django.conf import settings
+from .models import *
 
 
 def show_home(request):
-    return render(request, 'layout/home.html')
+    items_list = Items.objects.all()
+
+    return render(request, 'layout/home.html', {'items_list': items_list})
