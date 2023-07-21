34fdede55c57da8d38ed14aa9a1bbb7392a36933
Lucubrator <1318645809@qq.com>
2023-07-21 14:57:28 +0800

    增加了一个数据json文件，并利用json实现了数据库的插入，增加了商品图片

---
+++ utils/loaddata.py
@@ -0,0 +1,13 @@
+from item.models import *
+from django.db.transaction import atomic
+
+
+@atomic
+def load_model():
+    with open('utils/datas.json', encoding="utf-8", errors="ignore") as fr:
+        import json
+        datas = json.load(fr)
+
+        for data in datas:
+            item = Items.objects.create(gname=data['gname'], userid=data['userid'], price=data['price'],
+                                        intro_txt=data['intro_txt'], img_index=data['img_index'])
