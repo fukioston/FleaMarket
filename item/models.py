05ccfc7593a685f9021ef34ffb92c0b858532576
Lucubrator <1318645809@qq.com>
2023-07-21 09:04:10 +0800

    更改了items表的属性

--- item/models.py
+++ item/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Items(models.Model):
-    gid = models.IntegerField(verbose_name="商品id")
     gname = models.CharField(verbose_name="商品名", max_length=1000)
-    id = models.CharField(verbose_name="用户id")
+    userid = models.IntegerField(verbose_name="用户id")
     price = models.DecimalField(verbose_name="价格", max_digits=5, decimal_places=2)
     intro_txt = models.CharField(verbose_name="介绍文本", max_length=1000)
     img_index = models.CharField(verbose_name="图片路径", max_length=100)
