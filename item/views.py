5631864cbcc859365ba4009500be71e033d4fa0d
Otonashi <2442669412@qq.com>
2023-07-21 19:51:40 +0800

    #修复了商品列表为空时随机生成函数报错问题

--- item/views.py
+++ item/views.py
@@ -9,14 +9,15 @@ def show_home(request):
     list = Items.objects.all()
     ran = []
     i = 0
-    while True:
-        rn = randint(0, len(list) - 1)
-        if i == 12:
-            break
-        if rn not in ran:
-            ran.append(rn)
-            i = i + 1
-        finlist = []
+    finlist = []
+    if len(list)!=0:
+        while True:
+            rn = randint(0, len(list) - 1)
+            if i == 12:
+                break
+            if rn not in ran:
+                ran.append(rn)
+                i = i + 1
         for r in ran:
             finlist.append(list[int(r)])
     return render(request, 'layout/home.html', {'items_list': finlist})
