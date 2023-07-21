7143113a1580abfcf7b1c09788a49bad646eb86d
a6782533 <1311974687@qq.com>
2023-07-21 20:14:56 +0800

    完善商城右上角登录信息的显示

--- item/views.py
+++ item/views.py
@@ -1,6 +1,8 @@
 from item import templates
 from django.shortcuts import render, HttpResponse
 from django.conf import settings
+
+from user.models import UserInfo
 from .models import *
 from random import *
 
@@ -10,7 +12,7 @@ def show_home(request):
     ran = []
     i = 0
     finlist = []
-    if len(list)!=0:
+    if len(list) != 0:
         while True:
             rn = randint(0, len(list) - 1)
             if i == 12:
@@ -20,9 +22,17 @@ def show_home(request):
                 i = i + 1
         for r in ran:
             finlist.append(list[int(r)])
-    #检测当前页面是不是商城，是就不显示商城标签，将url传入item网页
+
+    # 检测当前页面是不是商城，是就不显示商城标签，将url传入item网页
     current_url = request.path
-    return render(request, 'layout/home.html', {'items_list': finlist, 'current_url': current_url})
+
+    # 获取当前session的用户是谁，如果有，右上角可以显示用户名
+    info = request.session.get('info')
+    user_id = info['id']
+    query_set = UserInfo.objects.filter(id=user_id).first()
+
+    return render(request, 'layout/home.html',
+                  {'items_list': finlist, 'current_url': current_url, 'user_info': query_set})
 
 
 def show_details(request, gid):
