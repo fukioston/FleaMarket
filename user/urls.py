f9e1213d004eb615444fb4dd228855d04401a899
a6782533 <1311974687@qq.com>
2023-07-21 10:41:06 +0800

    （branch_user）完成登录界面功能的编写

--- user/urls.py
+++ user/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 
 from django.urls import path
 from user.views import login
+
 urlpatterns = [
     path('login/', login.login),
-    path('sms/code/',login.send_sms),
-    path('login/sms/',login.login_sms)
+    path('sms/code/', login.send_sms),
+    path('login/sms/', login.login_sms)
 
 ]
