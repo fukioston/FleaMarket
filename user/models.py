3e5acd88afe4d17acf1f379e743bb6fef10f2369
a6782533 <1311974687@qq.com>
2023-07-21 08:55:28 +0800

    （branch_user）编写basic母版，创建login网页

--- user/models.py
+++ user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class UserInfo(models.Model):
     username = models.CharField(verbose_name='用户名', max_length=32)
     password = models.CharField(verbose_name='密码', max_length=32)
     email = models.EmailField(verbose_name='邮箱', max_length=32)
     mobile_phone = models.CharField(verbose_name='手机号', max_length=32)
-
