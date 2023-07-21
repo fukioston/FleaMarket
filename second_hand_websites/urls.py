9d9e57b2d9bdddc4cbe3d61593525b411063d582
fukioston <2112966@mail.nankai.edu.cn>
2023-07-22 00:28:34 +0800

    <branch_master>增加了在选择退出登录时的选择框，防止用户失手退出

--- second_hand_websites/urls.py
+++ second_hand_websites/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import item.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("item/", include('item.urls')),
-    path("user/", include('user.urls'))
+    path("user/", include('user.urls')),
 ]
