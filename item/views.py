21e7b6246a096b7f75b18212aae5e80ab5066b81
a6782533 <1311974687@qq.com>
2023-07-22 19:47:51 +0800

    #修改小细节

--- item/views.py
+++ item/views.py
@@ -19,8 +19,6 @@ def show_home(request):
     if request.method == 'POST':
         form = SearchForm(request, data=request.POST)
         kw = form['search_field'].value()
-        if kw == '陈高楠的鸡巴':
-            return HttpResponse('他没有鸡巴，你搜不到！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！')
         goods = Items.objects.filter(gname__contains=kw)
         if goods:
             print('has goods')
