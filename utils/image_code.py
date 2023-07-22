a97dec528f1ca1e0f1a85d8a7989c83fb6989da1
fukioston <2112966@mail.nankai.edu.cn>
2023-07-22 08:39:01 +0800

    <branch_master>修改图片验证码让大家更容易看懂

--- utils/image_code.py
+++ utils/image_code.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 
 
-def check_code(width=120, height=30, char_length=5, font_file='Monaco.ttf', font_size=28):
+def check_code(width=120, height=30, char_length=4, font_file='Monaco.ttf', font_size=28):
     code = []
     img = Image.new(mode='RGB', size=(width, height), color=(255, 255, 255))
     draw = ImageDraw.Draw(img, mode='RGB')
@@ -12,7 +12,7 @@ def check_code(width=120, height=30, char_length=5, font_file='Monaco.ttf', font
         生成随机字母
         :return:
         """
-        return chr(random.randint(65, 90))
+        return chr(random.randint(48, 57))
 
     def rndColor():
         """
